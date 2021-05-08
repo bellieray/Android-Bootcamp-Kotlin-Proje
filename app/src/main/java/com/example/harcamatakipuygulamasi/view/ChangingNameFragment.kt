e1c7bc2b49098b9215b2e8e86aa02cec893a5e4a
bellieray <erybelli@gmail.com>
2021-05-08 17:55:51 +0300

    Son degisikler tamamlandi

--- app/src/main/java/com/example/harcamatakipuygulamasi/view/ChangingNameFragment.kt
+++ app/src/main/java/com/example/harcamatakipuygulamasi/view/ChangingNameFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceManager
 import com.example.harcamatakipuygulamasi.R
 import com.google.android.material.snackbar.Snackbar
@@ -55,6 +56,10 @@ class ChangingNameFragment : Fragment() {
 
             }
 
+        isimEklemeBackButton.setOnClickListener {
+            val action = ChangingNameFragmentDirections.actionChangingNameFragmentToAnaEkranFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
             //İSİM KAYDETME İŞLEMLERİ
         buttonNameKaydet.setOnClickListener {
 
